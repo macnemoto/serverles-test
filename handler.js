c530c76e8f3993f3a3b62d4064a657db9f76b800
macnemoto <marcosj465@gmail.com>
2022-11-25 03:58:40 +0000

    :tada: first commit

---
+++ handler.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.helloUser = async (event) => {
+  //const { name } = path.Parameters
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `tu nombre es ${event.pathParameters.name}`,
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+}
+  //methodos post
+  module.exports.auth = async (event) => {
+    //const { name } = path.Parameters
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: `tu nombre es ${event.pathParameters.name}`,
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
